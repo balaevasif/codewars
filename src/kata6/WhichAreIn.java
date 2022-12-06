b7304f1541eef24e19e624e03b2a2c838d680925
Asif Balaev <balaev.job@gmail.com>
2022-12-06 19:10:02 +0400

    First solution 6 kata

---
+++ src/kata6/WhichAreIn.java
@@ -0,0 +1,26 @@
+package kata6;//https://www.codewars.com/kata/550554fd08b86f84fe000a58/train/java
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WhichAreIn {
+    public String[] inArray(String[] ar1, String[] ar2){
+
+        List<String> list = new ArrayList<>();
+        //for (int i = 0; i < ar1.length; i++) {
+        //  for (int j = 0; j < ar2.length; j++) {
+        for (String s : ar1) {
+            for (String value : ar2) {
+                if (value.contains(s)) {
+                    if (!list.contains(s)) {
+                        list.add(s.toLowerCase());
+                    }
+                }
+            }
+        }
+        String[] new_array = list.toArray(String[]::new);
+        Arrays.sort(new_array);
+        return new_array;
+    }
+}
