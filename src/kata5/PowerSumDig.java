c3e68fa2fb9731d5733cf8eadedde50059259d1e
Asif Balaev <balaev.job@gmail.com>
2022-12-07 17:03:39 +0400

    solved another one kata5, but again the same error, its takes so long time. I need to learn algoritms

---
+++ src/kata5/PowerSumDig.java
@@ -0,0 +1,36 @@
+package kata5;
+//https://www.codewars.com/kata/55f4e56315a375c1ed000159/train/java
+
+import java.util.ArrayList;
+import java.util.List;
+import static java.lang.Math.pow;
+
+
+public class PowerSumDig {
+
+    public static long powerSumDigTerm(int n) {
+        long max_value = Long.MAX_VALUE;
+        List<Integer> num = new ArrayList<>();
+
+        char[] numbers;
+        int test;
+        for (long i = 80; i < max_value; i++) {
+            test = 0;
+            numbers = Integer.toString((int) i).toCharArray();
+            for (int j = 0; j < numbers.length; j++) {
+                test += Integer.parseInt(String.valueOf(numbers[j]));
+            }
+            for (int j = 1; j < 5; j++) {
+                if (pow(test, j) == i) {
+                    num.add((int) i);
+                }
+            }
+            if (num.size()+1 > n) break;
+        }
+        return num.get(num.size()-1);
+    }
+
+    public static void main(String[] arg){
+        System.out.println(powerSumDigTerm(4));
+    }
+}
